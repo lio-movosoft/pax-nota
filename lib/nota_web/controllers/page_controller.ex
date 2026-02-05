654a054aa3296a5d7ea4697754d78ce6465fa4d0
Lio <masterkey@movo-soft.com>
2026-02-05 21:32:02 +0800

    :zap: [landing] a nice simple landing page

--- lib/nota_web/controllers/page_controller.ex
+++ lib/nota_web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule NotaWeb.PageController do
   use NotaWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    if conn.assigns[:current_scope] && conn.assigns.current_scope.user do
+      redirect(conn, to: ~p"/notes")
+    else
+      render(conn, :home)
+    end
+  end
+
+  def privacy_policy(conn, _params) do
+    render(conn, :privacy_policy)
   end
 end
